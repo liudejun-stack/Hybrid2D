d4270d7a6c7fa5fec054211e84b111f846e75ed3
joacasaraujo <engcjoaquim@gmail.com>
2019-05-28 19:19:49 -0300

    Pequenas modificações

--- Version 0.0.2/Fluid.hpp
+++ Version 0.0.2/Fluid.hpp
@@ -30,7 +30,7 @@ public:
 	//Pointers and Vectors:
     std::shared_ptr<D2Q9> D = std::make_shared<D2Q9>();             //Smart pointer to class D2Q9
     std::shared_ptr<Grid> G = std::make_shared<Grid>();             //Smart pointer to class Grid
-	std::vector<int> op = {0, 3, 4, 1, 2, 7, 8, 5, 6};				//Opposing function vector
+	std::vector<int> op = {0, 3, 4, 1, 2, 7, 8, 5, 6};				//Opposing node on bounce-back 
 	
 	//Fluid variables:
 	double tau;
