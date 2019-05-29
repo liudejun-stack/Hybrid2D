aab163d4bc5343f69b4b1e2423b41378df1922b8
joacasaraujo <engcjoaquim@gmail.com>
2019-05-28 21:23:49 -0300

    teste

--- Version 0.0.2/Lattice.cpp
+++ Version 0.0.2/Lattice.cpp
@@ -12,32 +12,32 @@ int Lattice::MapFunction(int i, int j, int k)
 
 void Lattice::setBoundary(bool _Top, bool _Bottom, bool _Left, bool _Right)
 {
-	for (int j = 0; j <= ny; j++) {
-		for (int i = 0; i <= nx; i++) {
+	for (int j = 0; j < ny; j++) {
+		for (int i = 0; i < nx; i++) {
 			Boundary[MapGrid(i, j)] = 0;
 		}
 	}
 
 	if (_Top) {
-		for (int i = 0; i <= nx; i++) {
+		for (int i = 0; i < nx; i++) {
 			Boundary[MapGrid(i, ny)] = 1;
 		}
 	}
 
 	if (_Bottom) {
-		for (int i = 0; i <= nx; i++) {
+		for (int i = 0; i < nx; i++) {
 			Boundary[MapGrid(i, 0)] = 1;
 		}
 	}
 
 	if (_Left) {
-		for (int j = 0; j <= ny; j++) {
+		for (int j = 0; j < ny; j++) {
 			Boundary[MapGrid(0, j)] = 1;
 		}
 	}
 
 	if (_Right) {
-		for (int j = 0; j <= ny; j++) {
+		for (int j = 0; j < ny; j++) {
 			Boundary[MapGrid(nx, j)] = 1;
 		}
 	}
