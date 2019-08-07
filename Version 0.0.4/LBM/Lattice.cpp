1b98ffb4131cb6f732cedae0b3173f563aa71406
joacasaraujo <engcjoaquim@gmail.com>
2019-08-07 15:06:04 -0300

    Changes

--- Version 0.0.4/LBM/Lattice.cpp
+++
@@ -1,7 +0,0 @@
-#include "Lattice.h"
-
-double Lattice::set_eqFun(double _rho, Vec2d _vel, int k) {
-	double cdotv = discreteVelocity[k].dot(_vel);
-	double vdotv = _vel.dot(_vel);
-	return weight[k] * _rho * (1 + 3 * cdotv / (latticeSpeed) + 4.5 * cdotv * cdotv / (latticeSpeed * latticeSpeed) - 1.5 * vdotv / (latticeSpeed * latticeSpeed));
-}
