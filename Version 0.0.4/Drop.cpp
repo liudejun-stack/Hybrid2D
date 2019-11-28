081e82054f892051e1502f4d2405e2c91ba9d552
joacasaraujo <engcjoaquim@gmail.com>
2019-11-28 08:48:25 -0300

    Geração dos resultados

--- Version 0.0.4/Drop.cpp
+++ Version 0.0.4/Drop.cpp
@@ -1,33 +1,33 @@
-#include "Scene.h"
-#include "Output.h"
-
-int main() {
-	Scene& S = S.getScene();
-	Output Out;
-	
-	//Geometry
-	S.domainSize = { 10, 10 };
-	//S.addCircle(0.5, 0.5, { 5, 2 }, { 0.0, 0.0 }, false);
-	S.addCircle(0.5, 0.5, { 5, 5 }, { 0.0, 0.0 }, true);
-
-	//Solid Properties:
-	S.frictionAngle = 30;
-	S.localDamping = 0;
-	S.factorOfSafety = 0.1;
-	S.normalStiffness = 1.0e6;
-	S.shearStiffness = 0.5e6;
-	S.simDuration = 100000;
-	
-	//Solver
-	S.prepareScenario();
-	int ignore = system("mkdir VTK_Solid");
-	for (int i = 0; i != S.simDuration; ++i) {
-		if (i % 1000 == 0) {
-			Out.displaySimulationInfo();
-		}
-		if (i % 100 == 0)	Out.solidVTK("DEM");
-		S.DEMEngine();
-	}
-
-	return 0;
-}
+//#include "Scene.h"
+//#include "Output.h"
+//
+//int main() {
+//	Scene& S = S.getScene();
+//	Output Out;
+//	
+//	//Geometry
+//	S.domainSize = { 10, 10 };
+//	//S.addCircle(0.5, 0.5, { 5, 2 }, { 0.0, 0.0 }, false);
+//	S.addCircle(0.5, 0.5, { 5, 5 }, { 0.0, 0.0 }, true);
+//
+//	//Solid Properties:
+//	S.frictionAngle = 30;
+//	S.localDamping = 0;
+//	S.factorOfSafety = 0.1;
+//	S.normalStiffness = 1.0e6;
+//	S.shearStiffness = 0.5e6;
+//	S.simDuration = 100000;
+//	
+//	//Solver
+//	S.prepareScenario();
+//	int ignore = system("mkdir VTK_Solid");
+//	for (int i = 0; i != S.simDuration; ++i) {
+//		if (i % 1000 == 0) {
+//			Out.displaySimulationInfo();
+//		}
+//		if (i % 100 == 0)	Out.solidVTK("DEM");
+//		S.DEMEngine();
+//	}
+//
+//	return 0;
+//}
